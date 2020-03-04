404c82974b02a1c27077616e41bf11cc23606b9b
Zach Wick <zachwick@Zachs-MacBook-Air.local>
2020-03-04 12:46:35 -0700

    added a button counter with .state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Person from './person_card/person_card';
 function App() {
   return (
     <div className="App">
-      <Person />
+      <Person first_name="Zach" last_name="Wick" age={1} hair_color="Black"/>
     </div>
   );
 }
--- src/person_card/person_card.js
+++ src/person_card/person_card.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
-const Header = (props) => (
-    <div>
-        <h1>{props.last_name}, {props.first_name}</h1>
-        <h4>Age: {props.age}</h4>
-        <h4>Hair Color: {props.hair_color}</h4>
-    </div>
-);
 class Person extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            clickCount: 0
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            clickCount: this.state.clickCount + 1
+        })
+    }
     render(){
-        return(<Header first_name="Zach" last_name="Wick" age="1" hair_color="Black"/>)
+        const Button = (props) => {
+            return (
+                <button onClick={this.handleClick}>Click to increase age</button>
+            );
+        }
+        return(
+            <div>
+                <h1>{this.props.last_name}, {this.props.first_name}</h1>
+                <h4>Age: {this.props.age + this.state.clickCount}</h4>
+                <h4>Hair Color: {this.props.hair_color}</h4>
+                <Button handleClick={this.handleClick}/>
+            </div>)
     }
 }
 export default Person;
